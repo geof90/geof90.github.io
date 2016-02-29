4a88cd17435d4a26722f8479fe924d2bda262811
Geoffrey Goh <kg422@cornell.edu>
2016-02-29 01:55:47 -0800

    add solver

--- rubiks/main.js
+++ rubiks/main.js
@@ -2,137 +2,74 @@ var locked = false;
 var OPS = ["F", "L", "B", "R", "U", "D"];
 var perspective = 0;
 
-function init() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / (window.innerHeight - 200), 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    var clock = new THREE.Clock();
+var solverInitialized = false;
+var solver = new Cube();
 
-    renderer.setClearColor(new THREE.Color(0xEEEEEE));
-    renderer.setSize(window.innerWidth, window.innerHeight - 200);
-    
-    var cubes = createCubes(scene);
-    
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-30, 20, 60);
-    spotLight.castShadow = true;
-    spotLight.intensity = 0.3;
-    scene.add(spotLight);
-    
-    var ambientLight = new THREE.AmbientLight(0xeeeeee);
-    scene.add(ambientLight);
-    
-    camera.position.x = -35;
-    camera.position.y = 17;
-    camera.position.z = 16;
-    
-    var orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
-    orbitControl.target = new THREE.Vector3(0, -3, 0);
-    orbitControl.enablePan = false;
-    
-    document.getElementById("viewport").appendChild(renderer.domElement);
-    
-    var tweener;
-    
-    window.onkeypress = function (e) {
-        switch (e.keyCode) {
-            case 70 /* F */:
-                tweener = action(cubes, scene, OPS[(0 + perspective) % 4]) || tweener;
-                break;
-            case 102 /* f */: 
-                tweener = action(cubes, scene, OPS[(0 + perspective) % 4], true) || tweener;
-                break;
-            case 76 /* L */:
-                tweener = action(cubes, scene, OPS[(1 + perspective) % 4]) || tweener;
-                break;
-            case 108 /* l */:
-                tweener = action(cubes, scene, OPS[(1 + perspective) % 4], true) || tweener;
-                break;
-            case 66 /* B */:
-                tweener = action(cubes, scene, OPS[(2 + perspective) % 4]) || tweener;
-                break;
-            case 98 /* b */:
-                tweener = action(cubes, scene, OPS[(2 + perspective) % 4], true) || tweener;
-                break;
-            case 82 /* R */:
-                tweener = action(cubes, scene, OPS[(3 + perspective) % 4]) || tweener;
-                break;
-            case 114 /* r */:
-                tweener = action(cubes, scene, OPS[(3 + perspective) % 4], true) || tweener;
-                break;
-            case 85 /* U */:
-                tweener = action(cubes, scene, OPS[4]) || tweener;
-                break;
-            case 117 /* u */:
-                tweener = action(cubes, scene, OPS[4], true) || tweener;
-                break;
-            case 68 /* D */:
-                tweener = action(cubes, scene, OPS[5]) || tweener;
-                break;
-            case 100 /* d */:
-                tweener = action(cubes, scene, OPS[5], true) || tweener;
-                break;
-        }
-    }
-
-    render();
-    function render() {
-        var delta = clock.getDelta();
-        orbitControl.update(delta);
-        var azimuthAngle = orbitControl.getAzimuthalAngle();
-        updatePerspective(azimuthAngle);
-        tweener && TWEEN.update();
-        requestAnimationFrame(render);
-        renderer.render(scene, camera);
-    }
-    
-    OPS.forEach(function(op, index) {
-        if (index < 4) {
-            document.getElementById(op).onclick = function() { tweener = action(cubes, scene, OPS[(index + perspective) % 4]) || tweener; }
-            document.getElementById(op.toLowerCase()).onclick = function() { tweener = action(cubes, scene, OPS[(index + perspective) % 4], true) || tweener; }
-        } else {
-            document.getElementById(op).onclick = function() { tweener = action(cubes, scene, op) || tweener; }
-            document.getElementById(op.toLowerCase()).onclick = function() { tweener = action(cubes, scene, op, true) || tweener; }
-        }
-    });
-        
-    function shuffle(cubes, scene) {
-        var randomOperations = [];
-        for (var i = 0; i < 20; i++) {
-            var rand = Math.random();
-            var randomOperation = OPS[Math.floor(rand * 6)];
-            randomOperations.push(randomOperation);
-        }
-        
-        var nextAction = function() {
-            randomOperations = randomOperations.splice(1);
-            if (randomOperations.length) {
-                tweener = action(cubes, scene, randomOperations[0], false, nextAction) || tweener;
-            }
-        };
-        
-        tweener = action(cubes, scene, randomOperations[0], false, nextAction);
-    }
-    
-    document.getElementById("shuffle").onclick = function() { shuffle(cubes, scene); };
-    document.getElementById("solve").onclick = function() { solve(cubes, scene); };
-    document.getElementById("rotate").onclick = function() { tweener = rotate(cubes, scene) || tweener; };
-}
+Cube.asyncInit("lib/cubejs/worker.js", function() {
+    solverInitialized = true;
+});
 
 window.onload = init;
 
-function updatePerspective(azimuthAngle) {
-    if (azimuthAngle < -Math.PI / 4 * 3) {
-        perspective = 1;
-    } else if (azimuthAngle < -Math.PI / 4) {
-        perspective = 0;
-    } else if (azimuthAngle < Math.PI / 4) {
-        perspective = 3;
-    } else if (azimuthAngle < Math.PI / 4 * 3) {
-        perspective = 2;
-    } else {
-        perspective = 1;
+function action(cubes, scene, operation, inverse, done) {
+    if (locked) return;
+    locked = true;
+    
+    solver.move(inverse ? (operation + "'") : operation);
+    
+    var axis = getAxis(operation, inverse);
+    var group = groupAndSwap(cubes, scene, operation, inverse);
+    
+    var curRotationAmount = 0;
+    var endRotationAmount = Math.PI / 2;
+    var tween = new TWEEN.Tween({ theta: curRotationAmount })
+        .to({ theta: endRotationAmount }, 100)
+        .onUpdate(function() {
+            group.rotateOnAxis(axis, this.theta - curRotationAmount);
+            curRotationAmount = this.theta;
+            if (this.theta === endRotationAmount) {
+                setTimeout(function() {
+                    scene.remove(group);
+                    var rotatedCubes = [];
+                    group.children.forEach(function(cube) {
+                        var worldPos = group.localToWorld(cube.position);
+                        cube.position.x = worldPos.x;
+                        cube.position.y = worldPos.y;
+                        cube.position.z = worldPos.z;
+                        rotatedCubes.push(cube);
+                    });
+                    
+                    rotatedCubes.forEach(function(cube) {
+                        rotateAroundWorldAxis(cube, axis, endRotationAmount);
+                        scene.add(cube);
+                    });
+                    
+                    locked = false;
+                    done && done();
+                }, 1);
+            }
+        })
+        .start();
+    
+    return tween;
+}
+
+function getAxis(operation, inverse) {
+    var operationToAxisMap = {};
+    operationToAxisMap[OPS[0]] = new THREE.Vector3(1, 0, 0);
+    operationToAxisMap[OPS[1]] = new THREE.Vector3(0, 0, 1);
+    operationToAxisMap[OPS[2]] = new THREE.Vector3(-1, 0, 0);
+    operationToAxisMap[OPS[3]] = new THREE.Vector3(0, 0, -1);
+    operationToAxisMap[OPS[4]] = new THREE.Vector3(0, -1, 0);
+    operationToAxisMap[OPS[5]] = new THREE.Vector3(0, 1, 0);
+    
+    var axis = operationToAxisMap[operation];
+    
+    if (inverse) {
+        return axis.multiplyScalar(-1);
     }
+    
+    return axis;
 }
 
 function createCubes(scene) {
@@ -300,71 +237,172 @@ function groupAndSwap(cubes, scene, operation, inverse) {
     return group;
 }
 
-function getAxis(operation, inverse) {
-    var operationToAxisMap = {};
-    operationToAxisMap[OPS[0]] = new THREE.Vector3(1, 0, 0);
-    operationToAxisMap[OPS[1]] = new THREE.Vector3(0, 0, 1);
-    operationToAxisMap[OPS[2]] = new THREE.Vector3(-1, 0, 0);
-    operationToAxisMap[OPS[3]] = new THREE.Vector3(0, 0, -1);
-    operationToAxisMap[OPS[4]] = new THREE.Vector3(0, -1, 0);
-    operationToAxisMap[OPS[5]] = new THREE.Vector3(0, 1, 0);
+function init() {
+    document.getElementById("overlay").style.display = "none";
     
-    var axis = operationToAxisMap[operation];
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / (window.innerHeight - 200), 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    var clock = new THREE.Clock();
+
+    renderer.setClearColor(new THREE.Color(0xEEEEEE));
+    renderer.setSize(window.innerWidth, window.innerHeight - 200);
     
-    if (inverse) {
-        return axis.multiplyScalar(-1);
+    var cubes = createCubes(scene);
+    
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-30, 20, 60);
+    spotLight.castShadow = true;
+    spotLight.intensity = 0.3;
+    scene.add(spotLight);
+    
+    var ambientLight = new THREE.AmbientLight(0xeeeeee);
+    scene.add(ambientLight);
+    
+    camera.position.x = -35;
+    camera.position.y = 17;
+    camera.position.z = 16;
+    
+    var orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
+    orbitControl.target = new THREE.Vector3(0, -3, 0);
+    orbitControl.enablePan = false;
+    
+    document.getElementById("viewport").appendChild(renderer.domElement);
+    
+    var tweener;
+    
+    window.onkeypress = function (e) {
+        switch (e.keyCode) {
+            case 70 /* F */:
+                tweener = action(cubes, scene, OPS[(0 + perspective) % 4]) || tweener;
+                break;
+            case 102 /* f */: 
+                tweener = action(cubes, scene, OPS[(0 + perspective) % 4], true) || tweener;
+                break;
+            case 76 /* L */:
+                tweener = action(cubes, scene, OPS[(1 + perspective) % 4]) || tweener;
+                break;
+            case 108 /* l */:
+                tweener = action(cubes, scene, OPS[(1 + perspective) % 4], true) || tweener;
+                break;
+            case 66 /* B */:
+                tweener = action(cubes, scene, OPS[(2 + perspective) % 4]) || tweener;
+                break;
+            case 98 /* b */:
+                tweener = action(cubes, scene, OPS[(2 + perspective) % 4], true) || tweener;
+                break;
+            case 82 /* R */:
+                tweener = action(cubes, scene, OPS[(3 + perspective) % 4]) || tweener;
+                break;
+            case 114 /* r */:
+                tweener = action(cubes, scene, OPS[(3 + perspective) % 4], true) || tweener;
+                break;
+            case 85 /* U */:
+                tweener = action(cubes, scene, OPS[4]) || tweener;
+                break;
+            case 117 /* u */:
+                tweener = action(cubes, scene, OPS[4], true) || tweener;
+                break;
+            case 68 /* D */:
+                tweener = action(cubes, scene, OPS[5]) || tweener;
+                break;
+            case 100 /* d */:
+                tweener = action(cubes, scene, OPS[5], true) || tweener;
+                break;
+        }
+    }
+
+    render();
+    function render() {
+        var delta = clock.getDelta();
+        orbitControl.update(delta);
+        var azimuthAngle = orbitControl.getAzimuthalAngle();
+        updatePerspective(azimuthAngle);
+        tweener && TWEEN.update();
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
     }
     
-    return axis;
-}
-
-function action(cubes, scene, operation, inverse, done) {
-    if (locked) return;
-    locked = true;
-    
-    var axis = getAxis(operation, inverse);
-    var group = groupAndSwap(cubes, scene, operation, inverse);
-    
-    var curRotationAmount = 0;
-    var endRotationAmount = Math.PI / 2;
-    var tween = new TWEEN.Tween({ theta: curRotationAmount })
-        .to({ theta: endRotationAmount }, 100)
-        .onUpdate(function() {
-            group.rotateOnAxis(axis, this.theta - curRotationAmount);
-            curRotationAmount = this.theta;
-            if (this.theta === endRotationAmount) {
-                setTimeout(function() {
-                    scene.remove(group);
-                    var rotatedCubes = [];
-                    group.children.forEach(function(cube) {
-                        var worldPos = group.localToWorld(cube.position);
-                        cube.position.x = worldPos.x;
-                        cube.position.y = worldPos.y;
-                        cube.position.z = worldPos.z;
-                        rotatedCubes.push(cube);
-                    });
-                    
-                    rotatedCubes.forEach(function(cube) {
-                        rotateAroundWorldAxis(cube, axis, endRotationAmount);
-                        scene.add(cube);
-                    });
-                    
-                    locked = false;
-                    done && done();
-                }, 1);
+    OPS.forEach(function(op, index) {
+        if (index < 4) {
+            document.getElementById(op).onclick = function() { tweener = action(cubes, scene, OPS[(index + perspective) % 4]) || tweener; }
+            document.getElementById(op.toLowerCase()).onclick = function() { tweener = action(cubes, scene, OPS[(index + perspective) % 4], true) || tweener; }
+        } else {
+            document.getElementById(op).onclick = function() { tweener = action(cubes, scene, op) || tweener; }
+            document.getElementById(op.toLowerCase()).onclick = function() { tweener = action(cubes, scene, op, true) || tweener; }
+        }
+    });
+        
+    function shuffle(cubes, scene) {
+        var randomOperations = [];
+        for (var i = 0; i < 20; i++) {
+            var rand = Math.random();
+            var randomOperation = OPS[Math.floor(rand * 6)];
+            randomOperations.push(randomOperation);
+        }
+        
+        var nextAction = function() {
+            randomOperations = randomOperations.splice(1);
+            if (randomOperations.length) {
+                tweener = action(cubes, scene, randomOperations[0], false, nextAction) || tweener;
             }
-        })
-        .start();
+        };
+        
+        tweener = action(cubes, scene, randomOperations[0], false, nextAction);
+    }
     
-    return tween;
+    document.getElementById("shuffle").onclick = function() { shuffle(cubes, scene); };
+    document.getElementById("solve").onclick = function() {
+        function doSolve() {
+            if (solverInitialized) {
+                document.getElementById("overlay").style.display = "none";
+                
+                Cube.asyncSolve(solver, function(solutionString) {
+                    var solution = solutionString.split(" ");
+                    var nextAction = function() {
+                        if (solution.length) {
+                            if (solution[0].endsWith("'")) {
+                                tweener = action(cubes, scene, solution[0].charAt(0), true, nextAction);
+                            } else if (solution[0].endsWith("2")) {
+                                var move = solution[0].charAt(0);
+                                solution = solution.splice(1);
+                                solution.unshift(move);
+                                solution.unshift(move);
+                                tweener = action(cubes, scene, move, false, nextAction);
+                            } else {
+                                tweener = action(cubes, scene, solution[0], false, nextAction);
+                            }
+                            
+                            solution = solution.splice(1);
+                        }
+                    };
+                    
+                    nextAction();
+                }); 
+            } else {
+                document.getElementById("overlay").style.display = "";
+                setTimeout(doSolve, 100);
+            }
+        }
+        
+        setTimeout(doSolve, 1);
+    };
+    document.getElementById("rotate").onclick = function() { tweener = rotateWholeCube(cubes, scene) || tweener; };
+}
+             
+function rotateAroundWorldAxis(object, axis, radians) {
+    var rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+    rotWorldMatrix.multiply(object.matrix);
+    object.matrix = rotWorldMatrix;
+    object.rotation.setFromRotationMatrix(object.matrix);
 }
 
-function rotate(cubes, scene) {
+function rotateWholeCube(cubes, scene) {
     if (locked) return;
     locked = true;
     
-    var axis = getAxis("F", true);
-    
+    var axis = getAxis(OPS[0], true);
     var group = new THREE.Object3D();
     for (var i = 0; i < 2; i++) {
         var clone = cubes.map(function(cube) {
@@ -418,10 +456,16 @@ function rotate(cubes, scene) {
     return tween;
 }
 
-function rotateAroundWorldAxis(object, axis, radians) {
-    var rotWorldMatrix = new THREE.Matrix4();
-    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
-    rotWorldMatrix.multiply(object.matrix);
-    object.matrix = rotWorldMatrix;
-    object.rotation.setFromRotationMatrix(object.matrix);
+function updatePerspective(azimuthAngle) {
+    if (azimuthAngle < -Math.PI / 4 * 3) {
+        perspective = 1;
+    } else if (azimuthAngle < -Math.PI / 4) {
+        perspective = 0;
+    } else if (azimuthAngle < Math.PI / 4) {
+        perspective = 3;
+    } else if (azimuthAngle < Math.PI / 4 * 3) {
+        perspective = 2;
+    } else {
+        perspective = 1;
+    }
 }
